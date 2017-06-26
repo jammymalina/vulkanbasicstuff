6a2858bd0cc3e74447a2ea0744afb4df691f8bf8
Jantosik <jakub.jantosik@accenture.com>
2017-06-26 14:53:40 +0200

    Code wasn't tested, may contain syntax errors

--- src/vulkan_tools/vk_store.c
+++ src/vulkan_tools/vk_store.c
@@ -10,22 +10,19 @@
 
 #define NO_APP_CONFIG 0 
 #define INVALID_APP_CONFIG 1 
-#define VALID_APP_CONFIG  2
-#define PARSER_ERROR  3
+#define VALID_APP_CONFIG 2
+#define PARSER_ERROR 3
 
-static bool parse_long(const char *str, long *val) {
+static bool parse_unsigned_long(const char *str, unsigned long *val) {
 	char *temp;
-	bool success = true;
 	errno = 0;
-	*val = strtol(str, &temp, 0);
+	*val = strtoul(str, &temp, 0);
 
-	if (temp == str || *temp != '\0' ||  ((*val == LONG_MIN || *val == LONG_MAX) && errno == ERANGE))
-		success = false;
-
-	return success;
+	return !(temp == str || *temp != '\0' || ((*val == 0 || *val == ULONG_MAX) && errno == ERANGE) || 
+		*val > LONG_MAX);
 }
 
-static int parse_version_index(const char *version) {
+static int version_index_get(const char *version) {
 	if (strcmp(version, "major") == 0) {
 		return 0;
 	}
@@ -45,8 +42,6 @@ static bool node_key_equals(yaml_node_t *node, const char *str) {
 }
 
 static size_t node_scalar_value_get(yaml_node_t *node, char *dest, size_t dest_size) {
-	if (!node)
-		return 0;
 	if (node->type != YAML_SCALAR_NODE) 
 		return 0;
     if (node->data.scalar.length >= dest_size)
@@ -56,57 +51,272 @@ static size_t node_scalar_value_get(yaml_node_t *node, char *dest, size_t dest_s
 	return node->data.scalar.length + 1;	
 }	
 
-static int parse_application_node(yaml_document_t *document, yaml_node_t *node) {
+static int node_scalar_number_value_get(yaml_node_t *node, unsigned long *dest) {
+	if (node->type != YAML_SCALAR_NODE) 
+		return INVALID_APP_CONFIG; 
+	bool success = parse_unsigned_long(node->data.scalar.value, dest);
+	return success ? VALID_APP_CONFIG : INVALID_APP_CONFIG;
+}
+
+static int parse_version(yaml_document_t *document, yaml_node_t *node, long version[3]) {
 	if (node->type != YAML_MAPPING_NODE)
 		return INVALID_APP_CONFIG;
-	yaml_node_pair_t *iter;
-	yaml_node_t *next_node;
-	char value[STORE_FIELD_SIZE];
+	version[0] = version[1] = version[3] = -1; 
 	for (iter = node->data.mapping.pairs.start; iter < node->data.mapping.pairs.top; iter++) {
 		next_node = yaml_document_get_node(document, iter->key);
 		if (next_node) {
-			if (node_key_equals(next_node, "name")) {
+			if (node_key_equals(next_node, "major") || 
+				node_key_equals(next_node, "minor") || 
+				node_key_equals(next_node, "patch")) 
+			{
+				int index = version_index_get(next_node->data.scalar.value); 
+				if (index == -1) 
+					return INVALID_APP_CONFIG;
 				next_node = yaml_document_get_node(document, iter->value);
-				size_t size = node_scalar_value_get(next_node, value, STORE_FIELD_SIZE);
-				if (size == 0) {
-					error_log("Error while parsing the name property, the prop may be invalid.");
-				}
-			} else if (node_key_equals(next_node, "title")) {
-			} else if (node_key_equals(next_node, "version")) {
-			} else if (node_key_equals(next_node, "engine")) {
-			} else if (node_key_equals(next_node, "vulkan")) {
+				if (!next_node)
+					return PARSER_ERROR;
+				unsigned long value; 
+				int status = node_scalar_number_value_get(next_node, &value); 		
+				if (status != VALID_APP_CONFIG) {
+					return status;
+				}	
+				version[index] = value;
+			} else {
+				return INVALID_APP_CONFIG;
 			}
+		} else {
+			return PARSER_ERROR;
 		}
 	}
 	return VALID_APP_CONFIG;
 }
 
-static int parse_document(yaml_document_t *document) {
+static int parse_string_sequence(yaml_document_t *document, yaml_node_t *node, 
+	char **strings, size_t num_of_strings, size_t string_size)
+{
+	if (node->type != YAML_SEQUENCE_NODE)
+		return INVALID_APP_CONFIG;
+	yaml_node_item_t *iter;	
+	size_t i = 0;
+	for (iter = node->data.sequence.items.start; iter < node->data.sequence.items.top; iter++) {
+		next_node = yaml_document_get_node(document, *iter);
+		if (!next_node)
+			return PARSER_ERROR;
+		if (i > num_of_strings)
+			return INVALID_APP_CONFIG; 
+		size_t size = node_scalar_value_get(next_node, strings[i], string_size);
+		if (size == 0) {
+			error_log("Error while parsing the name property, the prop is invalid.");
+			return INVALID_APP_CONFIG;
+		}
+		i++;
+	}
+	return VALID_APP_CONFIG;
+}
+
+static int parse_vulkan_node(yaml_document_t *document, yaml_node_t *node, vulkan_config *vk_info) {
+	if (node->type != YAML_MAPPING_NODE) 
+		return INVALID_APP_CONFIG;
+	char extensions_values[MAX_VULKAN_EXTENSIONS][VK_MAX_EXTENSION_NAME_SIZE];
+	long version_value[3];
+	for (iter = node->data.mapping.pairs.start; iter < node->data.mapping.pairs.top; iter++) {
+		next_node = yaml_document_get_node(document, iter->key);
+		if (next_node) {
+			if (node_key_equals(next_node, "version")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the vulkan version node");
+					return PARSER_ERROR;
+				}
+				int status = parse_version(document, next_node, version_value);
+				if (status != VALID_APP_CONFIG) {
+					if (status == INVALID_APP_CONFIG)
+						error_log("Error while parsing the vulkan version property, 
+							the prop is invalid");
+					else 
+						error_log("Parser error, location: the vulkan version node.");
+					return status;
+				}
+				vk_info->desired_version[0] = version_value[0] != -1 ? 
+					version_value[0] : vk_info->desired_version[0]; 
+				vk_info->desired_version[1] = version_value[1] != -1 ? 
+					version_value[1] : vk_info->desired_version[1]; 
+				vk_info->desired_version[0] = version_value[0] != -1 ? 
+					version_value[2] : vk_info->desired_version[0]; 
+			} else if (node_key_equals(next_node, "extensions")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the vulkan extensions node");
+					return PARSER_ERROR;
+				}
+				int status = parse_string_sequence(docuemtn, next_node, extensions_values, 
+					MAX_VULKAN_EXTENSIONS, VK_MAX_EXTENSION_NAME_SIZE);
+			}
+		} else {
+			error_log("Parser error, location: the engine node");
+			return PARSER_ERROR;
+		}
+	}
+	return VALID_APP_CONFIG;
+}
+
+static int parse_engine_node(yaml_document_t *document, yaml_node_t *node, application_config *app_info) {
+	if (node->type != YAML_MAPPING_NODE) 
+		return INVALID_APP_CONFIG;
+	char value[STORE_FIELD_SIZE];
+	long version_value[3];
+	for (iter = node->data.mapping.pairs.start; iter < node->data.mapping.pairs.top; iter++) {
+		next_node = yaml_document_get_node(document, iter->key);
+		if (next_node) {
+			if (node_key_equals(next_node, "name")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the engine name node");
+					return PARSER_ERROR;
+				}
+				size_t size = node_scalar_value_get(next_node, value, STORE_FIELD_SIZE);
+				if (size == 0) {
+					error_log("Error while parsing the name property, the prop is invalid.");
+					return INVALID_APP_CONFIG;
+				}
+				strcpy(app_info->engine.name, value);
+			} else if (node_key_equals(next_node, "version")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the engine version node");
+					return PARSER_ERROR;
+				}
+				int status = parse_version(document, next_node, version_value);
+				if (status != VALID_APP_CONFIG) {
+					if (status == INVALID_APP_CONFIG)
+						error_log("Error while parsing the engine version property, 
+							the prop is invalid");
+					else 
+						error_log("Parser error, location: the engine version node.");
+					return status;
+				}
+				app_info->engine.version[0] = version_value[0] != -1 ? 
+					version_value[0] : app_info->engine.version[0]; 
+				app_info->engine.version[1] = version_value[1] != -1 ? 
+					version_value[1] : app_info->engine.version[1]; 
+				app_info->engine.version[2] = version_value[2] != -1 ? 
+					version_value[2] : app_info->engine.version[2]; 
+			}
+		} else {
+			error_log("Parser error, location: the engine node");
+			return PARSER_ERROR;
+		}
+	}
+	return VALID_APP_CONFIG;
+}
+
+static int parse_application_node(yaml_document_t *document, yaml_node_t *node, 
+	application_config *app_info, vulkan_config *vk_info) 
+{
+	if (node->type != YAML_MAPPING_NODE)
+		return INVALID_APP_CONFIG;
+	yaml_node_pair_t *iter;
+	yaml_node_t *next_node;
+	char value[STORE_FIELD_SIZE];
+	long version_value[3];
+	for (iter = node->data.mapping.pairs.start; iter < node->data.mapping.pairs.top; iter++) {
+		next_node = yaml_document_get_node(document, iter->key);
+		if (next_node) {
+			if (node_key_equals(next_node, "name")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the application name node");
+					return PARSER_ERROR;
+				}
+				size_t size = node_scalar_value_get(next_node, value, STORE_FIELD_SIZE);
+				if (size == 0) {
+					error_log("Error while parsing the name property, the prop is invalid.");
+					return INVALID_APP_CONFIG;
+				}
+				strcpy(app_info->name, value);
+			} else if (node_key_equals(next_node, "title")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the application title node");
+					return PARSER_ERROR;
+				}
+				size_t size = node_scalar_value_get(next_node, value, STORE_FIELD_SIZE);
+				if (size == 0) {
+					error_log("Error while parsing the title property, the prop is invalid.");
+					return INVALID_APP_CONFIG;
+				}
+				strcpy(app_info->title, value);
+			} else if (node_key_equals(next_node, "version")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the application version node");
+					return PARSER_ERROR;
+				}
+				int status = parse_version(document, next_node, version_value);
+				if (status != VALID_APP_CONFIG) {
+					error_log("Error while parsing the application version property, 
+						the prop may be invalid");
+					return status;
+				}
+				app_info->version[0] = version_value[0] != -1 ? version_value[0] : app_info->version[0]; 
+				app_info->version[1] = version_value[1] != -1 ? version_value[1] : app_info->version[1]; 
+				app_info->version[2] = version_value[2] != -1 ? version_value[2] : app_info->version[2]; 
+			} else if (node_key_equals(next_node, "engine")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the application engine node");
+					return PARSER_ERROR;
+				}
+				int status = parse_engine_node(document, next_node, app_info);
+				if (status != VALID_APP_CONFIG) {
+					error_log("Error while parsing the engine property.");
+					return status;
+				}
+			} else if (node_key_equals(next_node, "vulkan")) {
+				next_node = yaml_document_get_node(document, iter->value);
+				if (!next_node) {
+					error_log("Parser error, location: the application vulkan node");
+					return PARSER_ERROR;
+				}
+				int status = parse_vulkan_node(document, next_node, vk_info);
+				if (status != VALID_APP_CONFIG) {
+					error_log("'Error while parsing the vulkan property.");
+					return status;
+				}
+			}
+		} else {
+			error_log("Parser error, location: the application node.");
+			return PARSER_ERROR;
+		}
+	}
+	return VALID_APP_CONFIG;
+}
+
+static int parse_document(yaml_document_t *document, application_config *app_info, vulkan_config *vk_info) {
 	yaml_node_t *node = yaml_document_get_root_node(document);
 	if (!node) {
 		return PARSER_ERROR; 
 	}	
 	yaml_node_t *next_node;
 	yaml_node_pair_t *iter;	
-	switch (node->type) {		
-		case YAML_MAPPING_NODE:
-			for (iter = node->data.mapping.pairs.start; iter < node->data.mapping.pairs.top; iter++) {
-				next_node = yaml_document_get_node(document, iter->key); 
-				if (next_node) {
-					if (node_key_equals(next_node, "application")) {
-						next_node = yaml_document_get_node(document, iter->value);
-						if (!next_node) return PARSER_ERROR;
-						return parse_application_node(document, next_node);	
-					} 
-				} else {
-					debug_log("Couldn't find key node.");
-				}
+	if (node->type == YAML_MAPPING_NODE) {
+		for (iter = node->data.mapping.pairs.start; iter < node->data.mapping.pairs.top; iter++) {
+			next_node = yaml_document_get_node(document, iter->key); 
+			if (next_node) {
+				if (node_key_equals(next_node, "application")) {
+					next_node = yaml_document_get_node(document, iter->value);
+					if (!next_node) {
+						error_log("Parser error, location: the application value node.");
+						return PARSER_ERROR;
+					}
+					return parse_application_node(document, next_node, app_info, vk_info);	
+				} 
+			} else {
+				error_log("Parser error, location: the document.");
+				return PARSER_ERROR;
 			}
-			break; 
-		default: 
-			break;
-		
-	}	
+		}
+	} 		
 	return NO_APP_CONFIG;
 }
 
@@ -156,6 +366,9 @@ bool create_instance(const vk_functions *vk, vk_store *store, const char *config
 	bool success = true;			
 	bool done = false;
 
+	application_config app_info;
+	vulkan_config vk_info;
+
 	yaml_document_t document; 
 	while (!done) {
 		if (!yaml_parser_load(&parser, &document)) {
@@ -166,7 +379,7 @@ bool create_instance(const vk_functions *vk, vk_store *store, const char *config
 		done = !yaml_document_get_root_node(&document);
 		
 		if (!done) {
-			int status = parse_document(&document);
+			int status = parse_document(&document, &app_info, &vk_info);
 			switch (status) {
 				case PARSER_ERROR: 
 					error_log("Parsing error.");
--- src/vulkan_tools/vk_store.h
+++ src/vulkan_tools/vk_store.h
@@ -10,26 +10,34 @@
 #define STORE_FIELD_SIZE 512
 
 typedef struct {
+	char title[STORE_FIELD_SIZE]; 
+	char name[STORE_FIELD_SIZE]; 
+	unsigned long version[3];
 	struct {
-		uint32_t number; 
-		long semanticForm[3];
-	} apiversion;
+		char name[STORE_FIELD_SIZE]; 
+		unsigned long version[3];
+	} engine;
+} application_config;
+
+typedef struct {
+	uint32_t desired_extensions_count;
+	char desired_extensions[MAX_VULKAN_EXTENSIONS][VK_MAX_EXTENSION_NAME_SIZE];
+	unsigned long desired_version[3];
+} vulkan_config;
+
+typedef struct {
+	uint32_t apiVersion;
 
 	uint32_t extensions_count; 
 	VkExtensionProperties extensions[MAX_VULKAN_EXTENSIONS];
 
 	VkInstance instance;	
 
-	struct {
-		char title[STORE_FIELD_SIZE]; 
-		char name[STORE_FIELD_SIZE]; 
-		long version[3];
-		struct {
-			char name[STORE_FIELD_SIZE]; 
-			long version[3];
-		} engine;
-	} application_info;
+	application_config application_info;
 } vk_store; 
+
+void init_default_application_config(application_config *config);
+void copy_application_config(application_config *dest, application_config *src);
 
 bool load_extensions(const vk_functions *vk, vk_store *store);
 bool create_instance(const vk_functions *vk, vk_store *store, const char *config_filename); 
